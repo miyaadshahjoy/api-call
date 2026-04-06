1e4db98b16ce805abb01ba8fab237e311cbd481e
Miyaad Shah Joy <miyaadshahjoy@gmail.com>
2026-04-06 20:10:33 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const apiCall = async function (url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Create a worker function that will call the api after every 5 minutes
+
+setInterval(() => {
+  apiCall("https://docpatient-nexus.onrender.com/");
+}, 300000);
